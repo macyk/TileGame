ac5a44b2d4baac2bb45c59ee30a93766b1e7ea9d
Macy <macy.kuang@gmail.com>
2013-07-23 18:07:31 -0400

    * clean up

--- javascript-astar/demo/demo.js
+++ javascript-astar/demo/demo.js
@@ -1,7 +1,6 @@
-/* 	demo.js http://github.com/bgrins/javascript-astar
-	MIT License
-	
-	Set up the demo page for the A* Search
+/* 	
+    Author Macy Kuang
+    A Tile Matching Game
 */
 
 window.log = function(){
@@ -9,86 +8,16 @@ window.log = function(){
 		console.log( Array.prototype.slice.call(arguments) );
 	}
 };
+var speed           = 100; // the smaller the faster
+var totalScore      = 0;
+var matchingScore   = 500;
+var wallNum         = 0;
+var pathNum         = 1;
+var gameSize        = 10;
+var star_color      = null; // the selected color name
 
-var totalScore = 0;
-var matchingScore = 100;
-
-$(function() {
-
-    var $grid = $("#search_grid");
-
-    var opts = {
-        gridSize: 10,
-        debug: false,
-    };
-
-    var grid = new GraphSearch($grid, opts, astar.search);
-
-    $("#btnGenerate").click(function() {
-    	grid.initialize();
-    });
-});
-
-var css = { start: "start", finish: "finish", wall: "wall", active: "active",};
-var tiles = {orange: "color00",sky: "color01",navy: "color02",bush: "color03",brown: "color04",
-            grape: "color05", green: "color06",blue: "color07",red: "color08",yellow: "color09",
-            purple: "color10",pink: "color11",grey: "color12",fog: "color13",cold: "color14",
-            lime: "color15",mint: "color16",candy: "color17",
-            orange_2: "color00",sky_2: "color01",navy_2: "color02",bush_2: "color03",brown_2: "color04",
-            grape_2: "color05", green_2: "color06",blue_2: "color07",red_2: "color08",yellow_2: "color09",
-            purple_2: "color10",pink_2: "color11",grey_2: "color12",fog_2: "color13",cold_2: "color14",
-            lime_2: "color15",mint_2: "color16",candy_2: "color17",};
-
-function pickRandomProperty(obj) {
-    var result;
-    var count = 0;
-    for (var prop in obj){
-        if (Math.random() < 1/++count){
-           result = obj[prop];
-           
-           //log(prop);
-        }
-    } // remove it
-    deleteByValue(result, obj);
-    return result;
-}
-
-function deleteByValue(val, obj) {
-    for(var f in tileSet) {
-        if(obj.hasOwnProperty(f) && obj[f] == val) {
-            delete tileSet[f];
-            return;
-        }
-    }
-}
-
-function GraphSearch($graph, options, implementation) {
-    this.$graph = $graph;
-    this.search = implementation;
-    this.opts = $.extend({wallFrequency:.1, debug:true, gridSize:10}, options);
-    this.initialize();
-}
-GraphSearch.prototype.setOption = function(opt) {
-    this.opts = $.extend(this.opts, opt);
-    if(opt["debug"]||opt["debug"]==false) {
-        this.drawDebugInfo(opt["debug"]);
-    }
-};
-
-function clone(obj) {
-    if (null == obj || "object" != typeof obj) return obj;
-    var copy = obj.constructor();
-    for (var attr in obj) {
-        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
-    }
-    return copy;
-}
-
-GraphSearch.prototype.initialize = function() {
-    tileSet = clone(tiles);
-    var self = this;
-	this.grid = [];
-	var nodes = [
+// level 0 tiles, 1 empty
+var nodes   = [
         [1,1,1,1,1,1,1,1,1,1],
         [1,1,1,1,1,1,1,1,1,1],
         [1,1,0,0,0,0,0,0,1,1],
@@ -100,6 +29,78 @@ GraphSearch.prototype.initialize = function() {
         [1,1,1,1,1,1,1,1,1,1],
         [1,1,1,1,1,1,1,1,1,1]
     ];
+
+// styles for game play
+var css = { start: "start", finish: "finish", wall: "wall", active: "active",};
+// styles for tiles
+var tiles = {orange: "color00",sky: "color01",navy: "color02",bush: "color03",brown: "color04",
+            grape: "color05", green: "color06",blue: "color07",red: "color08",yellow: "color09",
+            purple: "color10",pink: "color11",grey: "color12",fog: "color13",cold: "color14",
+            lime: "color15",mint: "color16",candy: "color17",
+            orange_2: "color00",sky_2: "color01",navy_2: "color02",bush_2: "color03",brown_2: "color04",
+            grape_2: "color05", green_2: "color06",blue_2: "color07",red_2: "color08",yellow_2: "color09",
+            purple_2: "color10",pink_2: "color11",grey_2: "color12",fog_2: "color13",cold_2: "color14",
+            lime_2: "color15",mint_2: "color16",candy_2: "color17",};
+
+$(function() {
+    var $grid = $("#search_grid");
+    var opts = {
+        gridSize: gameSize,
+    };
+    var grid = new GraphSearch($grid, opts, astar.search);
+    $("#btnGenerate").click(function() {
+        grid.initialize();
+    });
+});
+
+// pick a random item from an object list, and then remove it
+function pickRandomProperty(obj) {
+    var result;
+    var count = 0;
+    for (var prop in obj){
+        if (Math.random() < 1/++count){
+           result = obj[prop];
+        }
+    }
+    deleteByValue(result, obj);
+    return result;
+}
+
+// remove an item from an object
+function deleteByValue(val, obj) {
+    for(var f in tileSet) {
+        if(obj.hasOwnProperty(f) && obj[f] == val) {
+            delete tileSet[f];
+            return;
+        }
+    }
+}
+
+// search
+function GraphSearch($graph, options, implementation) {
+    this.$graph = $graph;
+    this.search = implementation;
+    this.opts = $.extend({wallFrequency:.1, debug:true, gridSize:10}, options);
+    this.initialize();
+}
+
+// clone object value
+function clone(obj) {
+    if (null == obj || "object" != typeof obj) return obj;
+    var copy = obj.constructor();
+    for (var attr in obj) {
+        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+    }
+    return copy;
+}
+
+// build tiles
+GraphSearch.prototype.initialize = function() {
+    totalScore  = 0; // reset game score
+    star_color  = null; // set start color to null
+    tileSet     = clone(tiles);
+    var self    = this;
+	this.grid   = [];
 	var $graph = this.$graph;
 
 	$graph.empty();
@@ -111,7 +112,6 @@ GraphSearch.prototype.initialize = function() {
 
     for(var x=0;x<this.opts.gridSize;x++) {
         var $row = $("<div class='clear' />");
-
     	var nodeRow = nodes[x];
     	var gridRow = [];
 
@@ -124,10 +124,10 @@ GraphSearch.prototype.initialize = function() {
 
     		if(nodeRow[y] == 0) {
                 $cell.addClass(pickRandomProperty(tileSet));
-                // make a start
     		}
     		else  {
                 if (!startSet) {
+                    // make the first none tile cell a start point
                     $cell.addClass(css.start);
                     startSet = true;
                 }
@@ -144,14 +144,13 @@ GraphSearch.prototype.initialize = function() {
     this.$cells.click(function() { self.cellClicked($(this)) });
 };
 
+// get the color name string
 function getColorName(obj) {
     var classes = obj.attr("class");
     var start_index = classes.indexOf("color")+5;
     var color_code = classes.substring(start_index, start_index+2);
     return color_code;
 };
-
-var star_color = -1;
 
 GraphSearch.prototype.cellClicked = function($end) {
 
@@ -171,20 +170,10 @@ GraphSearch.prototype.cellClicked = function($end) {
         log("clicked on start...", $end);
         return;
     }
-
-    /*if($end.hasClass(css.start) == false && $end.hasClass(css.wall) == false){
-        log("clicked on empty space...", end);
-        return;
-    }*/
-
-    /*if($end.hasClass(css.wall)) {
-        $end.removeClass(css.wall);
-        end.type = 1;
-    }*/
     log("end: "+ end.type);
     log("start: "+ start.type);
-    if(end.type == 0){
-        end.type = 1;
+    if(end.type == wallNum){
+        end.type = pathNum;
         log("new start");
         var this_color = getColorName($end);
         if(this_color != star_color){
@@ -202,12 +191,10 @@ GraphSearch.prototype.cellClicked = function($end) {
             start = this.nodeFromElement($start);
         }
 
-        var sTime = new Date();
         var path = this.search(this.graph.nodes, start, end, this.opts.diagonal);
-        var fTime = new Date();
 
         if(!path || path.length == 0)   {
-            $("#message").text("couldn't find a path ("+(fTime-sTime)+"ms)");
+            $("#message").text("couldn't find a path.");
             this.animateNoPath();
             end.type = 0; // if there is no path, set back to wall
             this.$cells.removeClass(css.start);
@@ -219,37 +206,17 @@ GraphSearch.prototype.cellClicked = function($end) {
             $start.removeClass("color"+star_color);
             star_color = -1;
             totalScore += matchingScore;
-            if(totalScore == 1800)
+            if(totalScore == 9000)
             {
                 $("#message").text("Great Work!");
             }else{
                 $("#message").text("Score: " + totalScore);
             }
-            if(this.opts.debug) {
-                this.drawDebugInfo(this.opts.debug);
-            }
             this.animatePath(path);
         }
     }
 };
-GraphSearch.prototype.drawDebugInfo = function(show) {
-    this.$cells.html(" ");
-    var that = this;
-    if(show) {
-    	that.$cells.each(function(i) { 
-            var node = that.nodeFromElement($(this));
-    		var debug = false;
-            if (node.visited) {
-                debug = "F: " + node.f + "<br />G: " + node.g + "<br />H: " + node.h;
-            }
 
-    		if (debug) {
-    			$(this).html(debug);
-    		}
-    	});
-
-    }
-};
 GraphSearch.prototype.nodeFromElement = function($cell) {
     return this.graph.nodes[parseInt($cell.attr("x"))][parseInt($cell.attr("y"))];
 };
@@ -266,7 +233,7 @@ GraphSearch.prototype.animateNoPath = function() {
 };
 GraphSearch.prototype.animatePath = function(path) {
 	var grid = this.grid;
-	var timeout = 1000 / grid.length;
+	var timeout = speed / grid.length;
 	var elementFromNode = function(node) {
 		return grid[node.x][node.y];
 	};
